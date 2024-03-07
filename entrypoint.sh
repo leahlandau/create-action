488513e4b0542cc2d3b7aa8f2245b7470d5a92bc
LeaL <LeaL@skyvar.co.il>
2024-03-07 10:36:11 +0200

    workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,36 +5,36 @@
 # echo "time=$time" >> $GITHUB_OUTPUT
 previous_release_created_at=$(curl -s -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" "https://api.github.com/repos/${{ github.repository }}/releases/latest" | jq -r '.created_at')
 
-latest_tag=$(git describe --tags)
+# latest_tag=$(git describe --tags)
 
-previous_tag=$(git describe --tags $(git rev-list --tags --skip=1 --max-count=1))
+# previous_tag=$(git describe --tags $(git rev-list --tags --skip=1 --max-count=1))
 
-issues=$(curl -s -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" "https://api.github.com/repos/${{ github.repository }}/issues?state=closed&per_page=100&since=$previous_release_created_at")
+# issues=$(curl -s -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" "https://api.github.com/repos/${{ github.repository }}/issues?state=closed&per_page=100&since=$previous_release_created_at")
 
-issues_list=""
+# issues_list=""
 
-while IFS= read -r row; do
+# while IFS= read -r row; do
 
-    number=$(echo "$row" | jq -r '.number')
+#     number=$(echo "$row" | jq -r '.number')
 
-    title=$(echo "$row" | jq -r '.title')
+#     title=$(echo "$row" | jq -r '.title')
 
-    assignees=$(curl -s -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" "https://api.github.com/repos/${{ github.repository }}/issues/$number" | jq -r '.assignees[].login' | xargs)
+#     assignees=$(curl -s -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" "https://api.github.com/repos/${{ github.repository }}/issues/$number" | jq -r '.assignees[].login' | xargs)
 
-    url=$(echo "$row" | jq -r '.html_url')
+#     url=$(echo "$row" | jq -r '.html_url')
 
-    assignee_links=""
+#     assignee_links=""
 
-    for assignee in $assignees; do
+#     for assignee in $assignees; do
 
-        assignee_links="$assignee_links[@$assignee](https://github.com/$assignee) "
+#         assignee_links="$assignee_links[@$assignee](https://github.com/$assignee) "
 
-    done
+#     done
 
-    issues_list="${issues_list}- $title in [#$number]($url) by $assignee_links\n"
+#     issues_list="${issues_list}- $title in [#$number]($url) by $assignee_links\n"
 
-done < <(echo "$issues" | jq -c '.[]')
+# done < <(echo "$issues" | jq -c '.[]')
 
-release_id=$(curl -s -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" "https://api.github.com/repos/${{ github.repository }}/releases/tags/$latest_tag" | jq -r '.id')
+# release_id=$(curl -s -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" "https://api.github.com/repos/${{ github.repository }}/releases/tags/$latest_tag" | jq -r '.id')
 
-curl -X PATCH -H "Authorization: token ${{ secrets.GITHUB_TOKEN }}" -d '{"body": "Whats Changed:\n\n'"$issues_list"'"}' "https://api.github.com/repos/${{ github.repository }}/releases/$release_id"
+# curl -X PATCH -H "Authorization: token ${{ secrets.GITHUB_TOKEN }}" -d '{"body": "Whats Changed:\n\n'"$issues_list"'"}' "https://api.github.com/repos/${{ github.repository }}/releases/$release_id"
